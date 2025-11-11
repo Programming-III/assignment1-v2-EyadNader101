d66592aa8fd3bc0129db5eaf317b5ef94a5d5341
EyadNader101 <Eyadnader2007@gmail.com>
2025-11-11 09:26:16 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,16 +5,31 @@
 using namespace std;
 
 //#write Person class here
-
-
-
-
-
-
-
-
-
-
-
+class person {
+private:
+    string name;
+    int id;
+public:
+    person(string name, int id);
+    void setName(string name);
+    string getName();
+    void setId(int id);
+    int getId();
+    void display();
+    ~person();
+};
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
