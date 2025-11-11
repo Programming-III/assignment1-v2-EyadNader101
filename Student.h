e05670bb6948ca19bbb80ae5a7bb214ec7039def
EyadNader101 <Eyadnader2007@gmail.com>
2025-11-11 09:34:26 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,17 +5,32 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Student : public person {
+private:
+    string major;
+    int yearlevel;
+public:
+    Student(string name, string major, int yearlevel, int id):person(name,id);
+    void setMajor(string major);
+    string getMajor();
+    void setYearLevel(int yearlevel);
+    int getYearLevel();
+    void display();
+    ~Student();
+};
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
